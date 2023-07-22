a87626a5913db97491a9cc538e969900991ab097
Jake Millhiser <jmmillhiser@gmail.com>
2023-07-22 06:17:42 -0700

    clj: refactor, named variables.

--- 2022/clj/day1/src/elfCalorie.clj
+++ 2022/clj/day1/src/elfCalorie.clj
@@ -1,12 +1,15 @@
 (ns elfCalorie
   (:require [clojure.string :as str]))
 
-(def file-data (str/split (slurp "input.txt") #"\r?\n\n"))                      ;; split input data based on two newlines
+(defn process-elf-calories [data]
+  (->> data
+       (str/split-lines)
+       (map #(read-string %))
+       (reduce +)))
+
+(def elf-calorie-data (str/split (slurp "input.txt") #"\r?\n\n"))
+(def elf-calories (mapv process-elf-calories elf-calorie-data))
 
 (defn -main []
-  (let [result
-        (mapv #(reduce + (mapv read-string (str/split-lines %))) file-data)]    ;; split data again, but eval as 
-                                                                                ;; integers and reduce the sum 
-
-    (println (reduce max result))                                               ;; part 1: highest
-    (println (reduce + (take 3 (sort > result))))))                             ;; part 2: sum of 3 highest
+  (println (reduce max elf-calories))
+  (println (reduce + (take 3 (sort > elf-calories)))))
