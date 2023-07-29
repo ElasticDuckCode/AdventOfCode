eb169e17296f29acb53db4cf3897370b258a9ba2
Jake Millhiser <jmmillhiser@gmail.com>
2023-07-28 17:50:16 -0700

    clj day4, starting day5

--- 2022/clj/advent/src/advent/core.clj
+++ 2022/clj/advent/src/advent/core.clj
@@ -2,12 +2,16 @@
   (:gen-class)
   (:require [advent.day1 :as day1])
   (:require [advent.day2 :as day2])
-  (:require [advent.day3 :as day3]))
+  (:require [advent.day3 :as day3])
+  (:require [advent.day4 :as day4]))
 
 (defn -main []
   (println "day1: ")
   (day1/solve-day1)
-  (println "day2: ")
+  (println "\nday2: ")
   (day2/solve-day2)
-  (println "day3: ")
-  (day3/solve-day3))
+  (println "\nday3: ")
+  (day3/solve-day3)
+  (println "\nday4: ")
+  (day4/solve-day4))
+
---
+++ 2022/clj/advent/src/advent/day4.clj
@@ -0,0 +1,39 @@
+(ns advent.day4
+  (:require [clojure.string :as str])
+  (:require [clojure.set :as set]))
+
+(defn is-superset
+  [l1 h1 l2 h2]
+  (if (or (and (<= l1 l2) (>= h1 h2)) (and (<= l2 l1) (>= h2 h1)))
+    1
+    0))
+
+;; TODO
+(defn has-overlap
+  [l1 h1 l2 h2]
+  (if (and (>= h1 l2) (<= l1 h2))
+    1
+    0))
+
+(defn count-overlaps
+  [[l1 h1 l2 h2 & rest]]
+  (if rest
+    (+ (has-overlap l1 h1 l2 h2) (count-overlaps rest))
+    (has-overlap l1 h1 l2 h2)))
+
+(defn count-supersets
+  [[l1 h1 l2 h2 & rest]]
+  (if rest
+    (+ (is-superset l1 h1 l2 h2) (count-supersets rest))
+    (is-superset l1 h1 l2 h2)))
+
+(defn solve-day4 []
+  (let [parsed-input (->> (slurp "src/advent/inputs/input4.txt")
+                          (str/split-lines)
+                          (mapv #(str/split % #"[,-]+"))
+                          (flatten)
+                          (mapv #(Integer/parseInt %)))]
+    (println (count-supersets parsed-input))
+    (println (count-overlaps parsed-input))))
+
+
