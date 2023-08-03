b553459deac9dcb7580471b8a372f1695073023e
Jake Millhiser <jmmillhiser@gmail.com>
2023-08-03 06:58:28 -0700

    replace recursive calls with recur

--- 2022/clj/advent/src/advent/day7.clj
+++ 2022/clj/advent/src/advent/day7.clj
@@ -30,17 +30,17 @@
   (let [dirs (assoc dirs pwd (+ fsize (get dirs pwd)))]
     (if (= pwd "/")
       dirs
-      (process-file (path-cd-prev pwd) dirs fname fsize))))
+      (recur (path-cd-prev pwd) dirs fname fsize))))
 
 (defn get-directory-sizes [pwd dirs [cmd & cmds]]
   (case (first cmd)
     nil dirs
     "$" (let [pwd (process-cmd pwd (rest cmd))]
-          (get-directory-sizes pwd dirs cmds))
+          (recur pwd dirs cmds))
     "dir" (let [dirs (process-dir pwd dirs (nth cmd 1))]
-            (get-directory-sizes pwd dirs cmds))
+            (recur pwd dirs cmds))
     (let [dirs (process-file pwd dirs (nth cmd 1) (Integer/parseInt (nth cmd 0)))]
-      (get-directory-sizes pwd dirs cmds))))
+      (recur pwd dirs cmds))))
 
 (defn solve-day7 []
   (let [input (slurp "src/advent/inputs/input7.txt")
@@ -62,16 +62,3 @@
          (filter (fn [x] (>= x delete-at-least)))
          (#(apply min %))
          (println))))
-
-(comment (reduce + (filter (fn [x] (<= x 100000)) (vals (solve-day7))))
-         (= "/" "/")
-         (process-file "/apple" {"/" 0 "/apple" 0} "bingus" 32840)
-         (first nil)
-         (apply path-join (butlast (str/split "/apple/pan", #"/")))
-         (path-cd-prev "/")
-         (path-cd-prev "/apple")
-         (keyword "/apple")
-         (let [my-map {"/" 0 "/apple" 5 "/pear" 10}]
-           (filter
-            #(str/starts-with? % "/apple")
-            (keys (assoc my-map "/apple/bottom" 0)))))
