0c32084ea3276c6335ea8ebe8fdb7471fa8bd39a
Jake Millhiser <jmmillhiser@gmail.com>
2023-08-06 09:55:13 -0700

    day10 part2, suprised I didn't need refactor.

--- 2022/clj/advent/src/advent/day10.clj
+++ 2022/clj/advent/src/advent/day10.clj
@@ -11,13 +11,27 @@
      (re-find #"addx" instr) (let [v (Integer/parseInt (last (str/split instr #"\s+")))]
                                (execute-device-asm instrs (conj x (last x) (+ (last x) v)))))))
 
+(defn draw-crt
+  ([clock x] (draw-crt clock x ""))
+  ([clock x screen]
+   (if (or (empty? clock) (empty? x))
+     screen
+     (let [horz (mod (first clock) 40)
+           screen (if (zero? horz) (str screen "\n") screen)
+           diff (abs (- horz (first x)))]
+       (cond
+         (<= diff 1) (recur (rest clock) (rest x) (str screen "#"))
+         :else (recur (rest clock) (rest x) (str screen ".")))))))
+
 (defn solve-day10 []
   (let [input (->> (slurp "src/advent/inputs/input10.txt")
                    (str/split-lines))
         reg-x (execute-device-asm input)
-        clock (mapv #(+ % 1) (vec (range (count reg-x))))
-        power (mapv * reg-x clock)]
-    (reduce + (mapv power [19 59 99 139 179 219]))))
+        clock (vec (range (count reg-x)))
+        power (mapv * reg-x (mapv #(+ % 1) clock))]
+    (println (reduce + (mapv power [19 59 99 139 179 219])))
+    (println (draw-crt clock reg-x))))
 
 (comment (solve-day10)
-         (re-find #"addx" "noop"))
+         (re-find #"addx" "noop")
+         (draw-crt (range 10) (take 10 (repeatedly #(rand-int 64)))))
