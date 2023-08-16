d1cddff37bd684966aca77c3b6f6a194900e7d11
Jake Millhiser <jmmillhiser@gmail.com>
2023-08-16 07:56:04 -0700

    day12 part 2 in clj. Brute forced using filter on "a" that have an "b" directly neighboring. Also first time using clojure pmap (parallel map).

--- 2022/clj/advent/src/advent/day12.clj
+++ 2022/clj/advent/src/advent/day12.clj
@@ -6,8 +6,22 @@
 (defn unwrap-index [idx n-col]
   [(quot idx n-col) (mod idx n-col)])
 
-(defn find-val [grid ch]
-  (unwrap-index (.indexOf (apply concat grid) ch) (count (get grid 0))))
+(defn flatten-if-single [s]
+  (if (= 1 (count s)) (flatten s) s))
+
+(defn find-val
+  ([grid ch]
+   (find-val grid ch 1))
+  ([grid ch n]
+   (->> grid
+        (apply concat)
+        (map-indexed vector)
+        (filter #(= (second %) ch))
+        (map first)
+        (map #(unwrap-index % (count (get grid 0))))
+        (#(case n
+            "all" %
+            (flatten-if-single (take n %)))))))
 
 (defn grid-to-map [grid]
   (let [m (count grid)
@@ -18,10 +32,10 @@
                    (for [j (range n)]
                      [[i j] (get-element grid i j)]))))))
 
-(defn initial-distance [grid]
+(defn initial-distance [grid start]
   (let [m (count grid)
         n (count (first grid))
-        [i j] (find-val grid \S)]
+        [i j] start]
     (grid-to-map
      (assoc (infs [m n]) i (assoc (vec (repeat n ##Inf)) j 0)))))
 
@@ -30,6 +44,11 @@
         h2 (if-not (= h2 \E) (int h2) (int \z))
         d (- h2 h1)]
     (if (>= d 2) ##Inf (max d 1))))
+
+(defn calculate-raw-distance [h1 h2]
+  (let [h1 (if-not (= h1 \S) (int h1) (int \a))
+        h2 (if-not (= h2 \E) (int h2) (int \z))]
+    (- h2 h1)))
 
 (defn calculate-grid-distance [grid p1 p2]
   (calculate-distance (get-element grid (first p1) (last p1))
@@ -48,6 +67,17 @@
   (if-not (nil? point)
     (recur (get prev point) prev (conj path point))
     path))
+
+(defn has-ascending-neighbor [grid point]
+  (let [[m n] [(count grid) (count (first grid))]
+        [i j] point
+        h (get-element grid (first point) (last point))
+        f (partial calculate-raw-distance h)]
+    (or
+     (if (< (inc i) m) (= 1 (f (get-element grid (inc i) j))) false)
+     (if (< (inc j) n) (= 1 (f (get-element grid i (inc j)))) false)
+     (if (>= (dec i) 0) (= 1 (f (get-element grid (dec i) j))) false)
+     (if (>= (dec j) 0) (= 1 (f (get-element grid i (dec j)))) false))))
 
 (defn dijkstra-hillclimb [grid end dist-map prev]
   (if (pos? (count dist-map))
@@ -78,14 +108,28 @@
                   (mapv char-array))
         path (dijkstra-hillclimb grid
                                  (find-val grid \E)
-                                 (initial-distance grid)
-                                 {(find-val grid \S) nil})]
-    (dec (count path))))
+                                 (initial-distance grid (find-val grid \S))
+                                 {(find-val grid \S) nil})
+        ;; NOTE: filters here are specific to my input, where most \a are local 
+        ;; islands with no way to climb.
+        paths (->> (find-val grid \a "all")
+                   (filter (fn [x] (has-ascending-neighbor grid x)))
+                   ;PMAP FTW
+                   (pmap #(dijkstra-hillclimb grid
+                                              (find-val grid \E)
+                                              (initial-distance grid %)
+                                              {% nil})))]
+    (map dec [(count path) (reduce min (map count paths))])))
 
 (comment (solve-day12)
          (calculate-distance \a \a)
          (calculate-distance \a \b)
          (calculate-distance \a \c)
          (repeat 10 ##Inf)
+         (let [grid (->> (slurp "src/advent/inputs/input12.txt")
+                         (str/split-lines)
+                         (mapv char-array))]
+           (filter (fn [x] (has-ascending-neighbor grid x))
+                   (find-val grid \a "all")))
          (infs [1 10]))
 
