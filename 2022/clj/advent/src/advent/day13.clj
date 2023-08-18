747146365677639c9465df522d9395027faca8c5
Jake Millhiser <jmmillhiser@gmail.com>
2023-08-17 18:53:17 -0700

    day13 part 1 clj

---
+++ 2022/clj/advent/src/advent/day13.clj
@@ -0,0 +1,41 @@
+(ns advent.day13
+  (:require
+   [clojure.string :as str]))
+
+(defn process-packet-pair [[x & xs] [y & ys]]
+  (println x y)
+  (cond
+    (every? integer? [x y]) (if (= x y) (recur xs ys) (< x y))
+    (every? coll? [x y]) (let [result (process-packet-pair x y)]
+                           (if (nil? result) (recur xs ys) result))
+    (and (integer? x) (coll? y)) (let [result (process-packet-pair [x] y)]
+                                   (if (nil? result) (recur xs ys) result))
+    (and (coll? x) (integer? y)) (let [result (process-packet-pair x [y])]
+                                   (if (nil? result) (recur xs ys) result))
+    (and (nil? x) (not (nil? y))) true
+    (nil? x) (if (every? nil? [xs ys]) nil (recur xs ys))
+    :else false))
+
+(defn compare-packet-data
+  ([pairs] (compare-packet-data [] pairs))
+  ([flags [[x y] & pairs]]
+   (println "--" x "-" y "--")
+   (if-not (or (nil? x) (nil? y))
+     (let [result (process-packet-pair x y)]
+       (println result)
+       (recur (conj flags result) pairs))
+     flags)))
+
+(defn solve-day13 []
+  (->> (slurp "src/advent/inputs/input13.txt")
+       (#(str/split % #"\n\n"))
+       (map #(str/split-lines %))
+       ((fn [pairs] (for [pair pairs] (map #(eval (read-string %)) pair))))
+       (compare-packet-data)
+       (map-indexed vector)
+       (filter (fn [x] (true? (second x))))
+       (map first)
+       (map inc)
+       (reduce +)))
+
+(comment (solve-day13))
